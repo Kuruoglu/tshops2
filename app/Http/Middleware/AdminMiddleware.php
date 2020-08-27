3322670915534a60dae11477ee8d985f7bc20bd6
Kuruoglu Andrey <7395836@gmail.com>
2020-08-27 15:49:31 +0300

    add AdminLte

--- app/Http/Middleware/AdminMiddleware.php
+++ app/Http/Middleware/AdminMiddleware.php
@@ -16,7 +16,7 @@ class AdminMiddleware
     public function handle($request, Closure $next)
     {
         if (!\Auth::user()->isAdmin) {
-            redirect('/login');
+             return redirect('/login');
         }
         return $next($request);
     }
